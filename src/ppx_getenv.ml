590647fcec25ebb51e7528e6b996d57a3bb6b6d4
Kate <kit.ty.kate@disroot.org>
2020-08-11 18:49:23 +0200

    Switch from OMP to ppxlib

--- src/ppx_getenv.ml
+++ src/ppx_getenv.ml
@@ -1,19 +1,13 @@
-open Migrate_parsetree.OCaml_411.Ast
-let ocaml_version = Migrate_parsetree.Versions.ocaml_411
-
-open Ast_mapper
-open Ast_helper
-open Asttypes
-open Parsetree
+open Ppxlib
+open Ppxlib.Ast_helper
 
 let getenv s = try Sys.getenv s with Not_found -> ""
 
-let getenv_mapper _config _cookies =
-  (* Our getenv_mapper only overrides the handling of expressions in the default mapper. *)
-  { default_mapper with
-    expr = fun mapper expr ->
-      match expr with
-      (* Is this an extension node? *)
+let traverse =
+  object
+    inherit Ppxlib.Ast_traverse.map as super
+
+    method! expression = function
       | { pexp_desc =
           (* Should have name "getenv". *)
           Pexp_extension ({ txt = "getenv"; loc }, pstr); _ } ->
@@ -21,15 +15,14 @@ let getenv_mapper _config _cookies =
         | (* Should have a single structure item, which is evaluation of a constant string. *)
           PStr [{ pstr_desc =
                   Pstr_eval ({ pexp_loc  = loc;
-                               pexp_desc = Pexp_constant (Pconst_string (sym, s_loc, None)); _ }, _); _ }] ->
+                               pexp_desc = Pexp_constant (Pconst_string (sym, None)); _ }, _); _ }] ->
           (* Replace with a constant string with the value from the environment. *)
-          Exp.constant ~loc (Pconst_string (getenv sym, s_loc, None))
+          Exp.constant ~loc (Pconst_string (getenv sym, None))
         | _ ->
-          raise (Location.Error (
-                  Location.error ~loc "[%getenv] accepts a string, e.g. [%getenv \"USER\"]"))
+          Location.raise_errorf ~loc "[%%getenv] accepts a string, e.g. [%%getenv \"USER\"]"
         end
       (* Delegate to the default mapper. *)
-      | x -> default_mapper.expr mapper x;
-  }
+      | expr -> super#expression expr
+  end
 
-let () = Migrate_parsetree.Driver.register ~name:"getenv" ocaml_version getenv_mapper
+let () = Ppxlib.Driver.register_transformation ~impl:traverse#structure "ppx_getenv"
