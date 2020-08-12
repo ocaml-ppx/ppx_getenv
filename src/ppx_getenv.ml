40485c3231d65ab575e0e93ba9187bb69f907b2e
Kate <kit.ty.kate@disroot.org>
2020-08-12 14:01:40 +0200

    Rewrite the ppx using rules to bring compatibility with ppx_const in dune

--- src/ppx_getenv.ml
+++ src/ppx_getenv.ml
@@ -3,26 +3,18 @@ open Ppxlib.Ast_helper
 
 let getenv s = try Sys.getenv s with Not_found -> ""
 
-let traverse =
-  object
-    inherit Ppxlib.Ast_traverse.map as super
+let expander ~loc ~path:_ = function
+  | (* Should have a single structure item, which is evaluation of a constant string. *)
+    PStr [{ pstr_desc =
+            Pstr_eval ({ pexp_loc  = loc;
+                         pexp_desc = Pexp_constant (Pconst_string (sym, None)); _ }, _); _ }] ->
+      (* Replace with a constant string with the value from the environment. *)
+      Exp.constant ~loc (Pconst_string (getenv sym, None))
+  | _ ->
+      Location.raise_errorf ~loc "[%%getenv] accepts a string, e.g. [%%getenv \"USER\"]"
 
-    method! expression = function
-      | { pexp_desc =
-          (* Should have name "getenv". *)
-          Pexp_extension ({ txt = "getenv"; loc }, pstr); _ } ->
-        begin match pstr with
-        | (* Should have a single structure item, which is evaluation of a constant string. *)
-          PStr [{ pstr_desc =
-                  Pstr_eval ({ pexp_loc  = loc;
-                               pexp_desc = Pexp_constant (Pconst_string (sym, None)); _ }, _); _ }] ->
-          (* Replace with a constant string with the value from the environment. *)
-          Exp.constant ~loc (Pconst_string (getenv sym, None))
-        | _ ->
-          Location.raise_errorf ~loc "[%%getenv] accepts a string, e.g. [%%getenv \"USER\"]"
-        end
-      (* Delegate to the default mapper. *)
-      | expr -> super#expression expr
-  end
+let extension =
+  Context_free.Rule.extension
+    (Extension.declare "getenv" Expression Ast_pattern.(__) expander)
 
-let () = Ppxlib.Driver.register_transformation ~impl:traverse#structure "ppx_getenv"
+let () = Ppxlib.Driver.register_transformation ~rules:[extension] "ppx_getenv"
